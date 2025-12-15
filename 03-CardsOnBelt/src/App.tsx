a8d43c094a41f1d5652a415d963f6fbd4ea61179
Franco M <franco.mgr@gmail.com>
2025-12-15 11:18:03 -0300

    Cards On Belt

---
+++ 03-CardsOnBelt/src/App.tsx
@@ -0,0 +1,11 @@
+import {CardsOnBelt} from "./belt/CardsOnBelt.tsx";
+
+function App() {
+    return (
+        <>
+            <CardsOnBelt/>
+        </>
+    )
+}
+
+export default App
---
+++ 03-CardsOnBelt/src/belt/CardsOnBelt.tsx
@@ -0,0 +1,5 @@
+export const CardsOnBelt = () => {
+    return (
+        <h1 className="text-green-500">Cards on Belt</h1>
+    )
+}
